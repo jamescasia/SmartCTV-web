243b572e30c9460381a577f8ecfb009debe5bf95
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-03-08 10:20:07 -0500

    Increment/decrement viewercount

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -4,6 +4,32 @@ import './App.css';
 
 import Auth from './components/Auth'
 class App extends Component {
+  
+  componentDidMount(){
+    this.updateViewerNum(1)
+    
+    /**
+     * decrement Viewer count on close.
+     */
+    window.addEventListener('beforeunload', (e) => {  
+      e.preventDefault();
+      this.updateViewerNum(-1)
+      return e.returnValue = 'Are you sure you want to close?';
+    });
+  }
+
+  componentWillUnmount(){
+    window.removeEventListener('beforeunload')
+  }
+
+  updateViewerNum = (updateBy) => {
+    this.props.database.ref('viewers').once('value').then(snap => {
+      this.props.database.ref().update({
+        viewers: snap.val() + updateBy,
+      })
+    })
+  }
+
   render() {
     return (
       <div className="App">
